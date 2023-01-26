1cb2f9df6a1480d9d9b667557adeecdd36d1f3c2
diamond5702 <122504389+diamond5702@users.noreply.github.com>
2023-01-25 19:32:37 -0500

    Update assignment_1.py

--- src/main/assignment_1.py
+++ src/main/assignment_1.py
@@ -1,1 +1,11 @@
-
+# Daina Diedrick
+# COT 4500
+# Assignment #1
+# January 25, 2023
+import numpy as np
+# Question #1: Use double precision, calculate the resulting values (format to 5 decimal places
+  # 010000000111111010111001
+  
+  s=0
+  c=2^10+2^2+2^1+2^0+2^(-1)+2^(-2)+2^(-3)+2^(-5)+2^(-7)+2^(-8)+2^(-9)+2^(-12)
+  double_pre=(-1)^s*2^(c-1027)(1+f)
