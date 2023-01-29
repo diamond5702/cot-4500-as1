08875eee356c9c36975bc475fe885e2da8b956bb
diamond5702 <122504389+diamond5702@users.noreply.github.com>
2023-01-28 23:14:44 -0500

    Update assignment_1.py

--- src/main/assignment_1.py
+++ src/main/assignment_1.py
@@ -5,12 +5,13 @@
 import numpy as np
 # Question #1: Use double precision, calculate the resulting values (format to 5 decimal places
   # 010000000111111010111001
-  
-  s=0
-  c=2^10+2^2+2^1+2^0
-  f=2^(-1)+2^(-2)+2^(-3)+2^(-5)+2^(-7)+2^(-8)+2^(-9)+2^(-12)
+def double_pre(s,c,f):
   double_pre=(-1)^s*2^(c-1027)(1+f)
-  return double_pre
-  print("/n")
+  return d
+s=0
+c=2^10+2^2+2^1+2^0
+f=2^(-1)+2^(-2)+2^(-3)+2^(-5)+2^(-7)+2^(-8)+2^(-9)+2^(-12)
+print(double_pre(s,c,f))
+print("/n")
 # Question #2: Chop
   
